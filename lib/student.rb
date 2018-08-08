a2852cb762438cb08b2f24dcb66d8216b3bed46e
Edsel Caprice <caprice@mail.usf.edu>
2018-08-08 23:10:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,12 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    @id = row[0]
-    @name = row[1]
-    @grade = row[2]
+    new_stuent = Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
   end
-
+  
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
