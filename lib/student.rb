813295dcb146abc8536b7948eaa50c1ad4d67a08
Edsel Caprice <caprice@mail.usf.edu>
2018-08-09 00:25:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,7 +79,7 @@ class Student
 
   def self.first_student_in_grade_10
     sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
-    DB[:conn].execute(sql)
+    self.new_from_db(DB[:conn].execute(sql).flatten)
   end
 
 
